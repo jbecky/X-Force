3ecbc275497eb255ce9f462552e025d91ebe91bc
jbecky <89868632+jbecky@users.noreply.github.com>
2023-09-26 01:11:04 -0400

    Update add_primary_names.py

--- add_primary_names.py
+++ add_primary_names.py
@@ -43,7 +43,7 @@ def get_url_2_all(url_1_all):
 def get_cas_no(url):
     soup = init_soup(url)
     tagged = soup.find_all(class_ = "card-text")
-    if len(tagged) > 1:  # Ensure the list has at least 2 elements
+    if len(tagged) > 1:  
         cas = tagged[1].text.split('\n')[1]
         cas = cas.replace('\r', '')
         cas = cas.strip()
@@ -55,25 +55,22 @@ def get_primary_name(url):
     soup = init_soup(url)
     h1 = soup.find_all("h1")
     if h1:
-        return h1[0].text.strip().upper()  # Use .text to get the text content of the tag, and then convert it to uppercase
+        return h1[0].text.strip().upper()  
     else:
         return "NOT FOUND"
     
 def build_name_dict():
     url_1_all = get_url_1_all()
     url_2_all = get_url_2_all(url_1_all)
-    prim_name_dict = {}  # Create a new dictionary to map CAS numbers to chemical names
+    prim_name_dict = {}  
     for url in url_2_all:
         cas = get_cas_no(url)
-        name = get_primary_name(url)  # Get the chemical name from the webpage
+        name = get_primary_name(url) 
         if name is not "NOT FOUND":
-            prim_name_dict[cas] = name  # Add the CAS number and chemical name to the dictionary
+            prim_name_dict[cas] = name 
         time.sleep(1)
     return prim_name_dict
 
-# Call the function to build the dictionary
 prim_name_dict = build_name_dict()
-
-# Save the dictionary to a JSON file
 with open('prim_name_dict.json', 'w') as f:
     json.dump(prim_name_dict, f)
